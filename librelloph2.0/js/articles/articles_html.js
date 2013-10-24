807b29f3fdb43f8370166fc8e25a055b2541ec05
Rodrigo De la Garza <josille@gmail.com>
2013-10-24 12:29:37 +0200

    html article, table padding added

--- librelloph2.0/js/articles/articles_html.js
+++ librelloph2.0/js/articles/articles_html.js
@@ -10,4 +10,31 @@ $( ".suplibrello" ).wrap( "<sup></sup>" );
  */
 $( ".sublibrello" ).wrap( "<sub></sub>" );
 
+
+
+/*
+On reference mouse over, display reference text
+a href="#Biblio
+*/
+
+$("a[href^='#Biblio']").mouseover(function () {
+	  //alert( $(this).attr('href') );
+	var idToCall = $(this).attr('href');
+		//alert($(idToCall).text());
+	$(this).attr('data-toggle','tooltip');
+	$(this).attr('title',$(idToCall).text());
+	$(this).attr('data-original-title',$(idToCall).text());
+	$(this).attr('placement','auto');
+	//	data-toggle="tooltip" title="first tooltip"
+	});
+
+
+/*
+$("a[href^='#Biblio']").each(function() {
+	var idToCall = $(this).attr('href');
+	$(this).attr('data-toggle','tooltip');
+	$(this).attr('title',$(idToCall).text());
+	$(this).attr('placement','auto');
+	});
+*/
 });
